ef83c926929bb4a96d9ebefecefba25fa815bedc
Andrey Azarov <azarov@anisoprint.ru>
2017-08-16 16:52:40 +0300

    Updated project

--- .ino.cpp
+++ .ino.cpp
@@ -2,7 +2,28 @@
 //This is a automatic generated file
 //Please do not modify this file
 //If you touch this file your change will be overwritten during the next build
-//This file has been generated on 2017-07-08 14:46:23
+//This file has been generated on 2017-08-16 16:52:19
+
+#include "Arduino.h"
+#include "base.h"
+#include <LiquidTWI2.h>
+#include <LCD.h>
+#include <LiquidCrystal_I2C.h>
+#include <LiquidCrystal_I2C.h>
+#include <U8glib.h>
+#include <LiquidCrystal.h>
+#include <Wire.h>
+#include <Wire.h>
+#include <Wire.h>
+#include <Wire.h>
+#include <TMC26XStepper.h>
+#include <TMC2130Stepper.h>
+#include <SPI.h>
+#include <SPI.h>
+#include <SPI.h>
+#include <SPI.h>
+#include <L6470.h>
+
 
 #include "MK4duo.ino"
 
