7c841342193dd1cbff6f74e48eddedffb8a2e47b
Andrey Azarov <azarov@anisoprint.ru>
2018-05-31 13:34:11 +0300

    Prusa plastic config updated

--- config/PRUSAI3_PLASTIC/Configuration_Basic.h
+++ config/PRUSAI3_PLASTIC/Configuration_Basic.h
@@ -123,7 +123,7 @@
  * See: https://github.com/MagoKimbra/MK4duo/blob/master/Documentation/Hardware.md *
  *                                                                                       *
  *****************************************************************************************/
-#define MOTHERBOARD BOARD_RAMPS_13_HHB
+#define MOTHERBOARD BOARD_RAMPS_13_HFB
 /*****************************************************************************************/
 
 
