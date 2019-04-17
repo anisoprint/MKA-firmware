25bd19d595774f91dce3fe75cbf4a187f983b61b
Andrey Azarov <azarov@anisoprint.ru>
2019-04-17 18:53:32 +0300

    Version update 0.9.2b

--- Configuration.h
+++ Configuration.h
@@ -1,25 +1,9 @@
 /**
  * Configuration.h
- *
- * This configuration file contains link to the printer settings
- *
- * SPUTNIX - CoreXY printer with 400x600x200 zone, MKS-GEN, ReprapDiscount graphics LCD, EG-3 extruder
- * PRUSAI3 - Cartesian printer with 200x200x200 zone, RAMPS 1.4, ReprapDiscount text LCD, EG-3 extruder - TEST
- * BIZON - Cartesian printer with 300x300x300 zone, MKS-GEN, ReprapDiscount text LCD, EG-3 extruder
- * PRUSAI3_PLASTIC - Cartesian printer with 200x200x200 zone, RAMPS 1.4, ReprapDiscount text LCD, plastic extruder
- *
- * COMPOSER_A4_KL 	- CoreXY printer with 297x210x149 zone, Megatronics 3.0, Nextion HMI, EG-5 extruder (manufactured by KL)
- * COMPOSER_A4_I 	- CoreXY printer with 297x210x149 zone, Ultratronics 1.0, Nextion HMI, EG-5 extruder (manufactured in Ivanovo by BL)
- * COMPOSER_A4_D 	- CoreXY printer with 297x210x149 zone, Ultratronics 1.0, Nextion HMI, EG-5 extruder (manufactured in Dubna)
- * COMPOSER_A4_T 	- CoreXY printer with 297x210x149 zone, Ultratronics 1.0, Nextion HMI, EG-6 extruder (manufactured in Troitsk)
- * COMPOSER_A4_V10 	- CoreXY printer with 297x210x145 zone, Ultratronics 1.0, Nextion HMI, EG-6 extruder (Serial, manufactured in Troitsk)
- *
  */
 
 #ifndef CONFIGURATION_H_
 #define CONFIGURATION_H_
-
-#define PRINTER_TYPE COMPOSER_A3_V090
 
 
 #define CONFIGURATION_OVERALL
